c0b12021b47a8e1902ef5d5735b2ad356faf2830
SurrealShock <Adrian_Avram@Outlook.com>
2018-08-06 15:49:08 -0700

    Added scheduler

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-var token = require('./DiscordToken.js');
+var token = require('./Discord_Token.js');
 const Discord = require("discord.js");
 const ud = require('urban-dictionary');
+var schedule = require('node-schedule');
 const bot = new Discord.Client();
 
 function Get(yourUrl) {
@@ -13,44 +14,55 @@ function Get(yourUrl) {
 
 bot.on("ready", () => {
   console.log(`Logged in as ${bot.user.tag}`);
-  bot.user.setActivity('!define')
+  bot.user.setActivity('f in chat boys')
+});
+
+var j = schedule.scheduleJob('00 * * * *', function(){
+  var json_obj = JSON.parse(
+            Get("https://www.reddit.com/r/dankmemes/hot.json")
+          );
+      var memeURL = json_obj.data.children[0].data.url;
+      var memeTitle = json_obj.data.children[0].data.title;
+  bot.channels.get("476157539013361684").send(memeTitle, {
+    file: memeURL
+  });
 });
 
 bot.on("message", message => {
-  // console.log("Channel: " + message.channel);
-  if (!message.author.bot) {
-    var msg = message.content;
-    msg = msg.toLowerCase();
-    if (msg == "!meme") {
-      var json_obj = JSON.parse(
-        Get("https://www.reddit.com/r/dankmemes/hot.json")
-      );
-      var randNum = Math.floor(Math.random() * 21);
-      var memeURL = json_obj.data.children[randNum].data.url;
-      var memeTitle = json_obj.data.children[randNum].data.title;
-      message.channel.send(memeTitle, {
-        file: memeURL
-      });
-    }
+  console.log("Channel: " + message.channel.id); });
+//   if (!message.author.bot) {
+//     var msg = message.content;
+//     msg = msg.toLowerCase();
+//     if (msg == "!meme") {
+//       var json_obj = JSON.parse(
+//         Get("https://www.reddit.com/r/dankmemes/hot.json")
+//       );
+//       var randNum = Math.floor(Math.random() * 21);
+//       var memeURL = json_obj.data.children[randNum].data.url;
+//       var memeTitle = json_obj.data.children[randNum].data.title;
+//       message.channel.send(memeTitle, {
+//         file: memeURL
+//       });
+//     }
 
-    if (msg.substring(0, 7) == "!define") {
-      msg.replace(/\s+/g, " ").trim()
-      msg.toLowerCase
-      if (msg.length == 7) {
-        message.channel.send("Use !define {term} to get the definition of a word from urban dictionary.");
-      } else {
-        var term = msg.substring(8, msg.length)
-        ud.term(term, function (error, entries, tags, sounds) {
-          if (error) {
-            message.channel.send("Could not find term: " + term);
-          } else {
-            message.channel.send(entries[0].word + ": " + entries[0].definition)
-            message.channel.send(entries[0].example)
-          }
-        })
-      }
-    }
-  }
-});
+//     if (msg.substring(0, 7) == "!define") {
+//       msg.replace(/\s+/g, " ").trim()
+//       msg.toLowerCase
+//       if (msg.length == 7) {
+//         message.channel.send("Use !define {term} to get the definition of a word from urban dictionary.");
+//       } else {
+//         var term = msg.substring(8, msg.length)
+//         ud.term(term, function (error, entries, tags, sounds) {
+//           if (error) {
+//             message.channel.send("Could not find term: " + term);
+//           } else {
+//             message.channel.send(entries[0].word + ": " + entries[0].definition)
+//             message.channel.send(entries[0].example)
+//           }
+//         })
+//       }
+//     }
+//   }
+// });
 
 bot.login(token);
