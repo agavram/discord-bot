20993706eb3725293f5a1ffc2814bfa8a1f5e13c
Adrian Avram <Adrian_Avram@Outlook.com>
2020-09-20 14:33:55 -0700

    Removed react

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -84,8 +84,6 @@ export default class Bot {
                 if (new RegExp("([a-zA-Z0-9]+://)?([a-zA-Z0-9_]+:[a-zA-Z0-9_]+@)?([a-zA-Z0-9.-]+\\.[A-Za-z]{2,4})(:[0-9]+)?(/.*)?").test(message.content) || message.attachments.size != 0) {
                         message.channel.send('Bad Sam no attachments or URLs');
                         message.delete();
-                } else {
-                    message.react('642834455790092318');
                 }
             }
 
