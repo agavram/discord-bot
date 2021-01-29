a759645763fd2f562ca94f1bc866e9c3e437ee4f
Adrian Avram <Adrian_Avram@Outlook.com>
2021-01-28 20:34:42 -0800

    le sigh

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -73,6 +73,9 @@ export default class Bot {
 
             let msg = message.content;
 
+            if (msg.toLowerCase() === "sigh")
+                message.channel.send("le sigh")
+
             if (!msg.startsWith(this.prefix))
                 return;
 
