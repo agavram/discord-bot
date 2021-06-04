1ee158f4bacc0381960b0b681333c74cdd8a9be5
SansDoge <alan.yyd@gmail.com>
2021-06-03 23:03:52 -0700

    Update bot.ts
    added !cum

--- src/bot/bot.ts
+++ src/bot/bot.ts
@@ -350,6 +350,10 @@ export default class Bot {
             }
         });
 
+        command.on("cum", (message: Message) => {
+            message.channel.send("8===D 💦");
+        });
+
         dm.on("channel", async (message: Message) => {
             const user: user = {
                 userId: message.author.id,
