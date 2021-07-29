1bd6dded6aa768cd2c6b5f313553b965f1168da7
Adrian Avram <Adrian_Avram@Outlook.com>
2021-07-28 19:54:50 -0700

    Formatting fixes

--- src/interfaces/database.ts
+++ src/interfaces/database.ts
@@ -1,25 +1,25 @@
 export interface server {
-    _id?: string,
-    server: string,
-    channelGeneral: string,
-    channelMemes: string,
-    channelLogging: string,
-    channelMariners?: string,
-    posts: Array<string>
+  _id?: string;
+  server: string;
+  channelGeneral: string;
+  channelMemes: string;
+  channelLogging: string;
+  channelMariners?: string;
+  posts: Array<string>;
 }
 
 export interface event {
-    _id?: string,
-    title: string,
-    time: Date,
-    attendees: Array<string>,
-    messageId: string,
-    channelId: string
+  _id?: string;
+  title: string;
+  time: Date;
+  attendees: Array<string>;
+  messageId: string;
+  channelId: string;
 }
 
 export interface user {
-    _id?: string,
-    userId: string,
-    channelAnon?: string,
-    sentAttachments?: number
+  _id?: string;
+  userId: string;
+  channelAnon?: string;
+  sentAttachments?: number;
 }
--- src/plugins/latex.ts
+++ src/plugins/latex.ts
@@ -1,25 +1,24 @@
 import * as mjAPI from 'mathjax-node-svg2png';
 
 export class LatexConverter {
-    public static async convert(latex: string) : Promise<Buffer> {
-        return new Promise((resolve, reject) => {
+  public static async convert(latex: string): Promise<Buffer> {
+    return new Promise((resolve, reject) => {
+      const options = {
+        math: `\\color{white}{${latex}}`,
+        format: 'TeX',
+        png: true,
+        scale: 3,
+      };
 
-            const options = {
-                math: `\\color{white}{${latex}}`,
-                format: 'TeX',
-                png: true,
-                scale: 3
-            };
-
-
-            mjAPI.typeset(options, result => {
-                if (!result.errors) {
-                    const pngString = result.png.replace(/^data:image\/png;base64,/, ""), image = Buffer.from(pngString, 'base64');
-                    resolve(image);
-                } else {
-                    reject(result.errors);
-                }
-            });
-        });
-    }
+      mjAPI.typeset(options, (result) => {
+        if (!result.errors) {
+          const pngString = result.png.replace(/^data:image\/png;base64,/, ''),
+            image = Buffer.from(pngString, 'base64');
+          resolve(image);
+        } else {
+          reject(result.errors);
+        }
+      });
+    });
+  }
 }
