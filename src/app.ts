a7a0a034d9536498b6ea3c3ed33616d7f2ec2d16
Adrian Avram <Adrian_Avram@Outlook.com>
2023-07-13 20:27:19 -0700

    Disconnect on close

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,17 @@
+import * as mongoose from 'mongoose';
 import Bot from './bot/bot';
 
 const bot: Bot = new Bot();
 
 bot.Ready.then(() => {
   console.log('Ready');
+});
+
+process.on('SIGINT', function() {
+  mongoose.disconnect();
+});
+
+
+process.on('SIGTERM', function() {
+  mongoose.disconnect();
 });
